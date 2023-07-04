5d98c4c3949f487a1e6d9b64848ddea434382f5b
marynvdl <maryn@laarse.co.za>
2023-07-04 10:09:32 +0200

    Have data directory persistent outside container

--- tasks/createBuiltTiles/createBuiltTiles.sh
+++ tasks/createBuiltTiles/createBuiltTiles.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 # Make the temporary data directory
-mkdir -p ./data/input
-mkdir -p ./data/colored
-mkdir -p ./data/masked
-mkdir -p ./data/output
+mkdir -p /container/data/input
+mkdir -p /container/data/colored
+mkdir -p /container/data/masked
+mkdir -p /container/data/output
 
 # Copy images from gc to local repo
-gsutil cp gs://nature-watch-bucket/COGS/built/built2022/built2022_16.tif ./data/input
+gsutil cp gs://nature-watch-bucket/COGS/built/built2022/built2022_16.tif /container/data/input
 
 # Apply a color-relief to the image
-gdaldem color-relief ./data/input/built2022_16.tif ./color.txt ./data/colored/colored.tif
+gdaldem color-relief /container/data/input/built2022_16.tif ./color.txt /container/data/colored/colored.tif
 
 # Mask no data values
-gdal_translate -a_nodata 0 ./data/colored/colored.tif ./data/masked/masked.tif
+gdal_translate -a_nodata 0 /container/data/colored/colored.tif /container/data/masked/masked.tif
 
 # Make tiles
-gdal2tiles.py -z 0-10 -s EPSG:4326 -r max -w none -a 0 --xyz ./data/masked/masked.tif ./data/output
+gdal2tiles.py -z 0-10 -s EPSG:4326 -r max -w none -a 0 --xyz /container/data/masked/masked.tif /container/data/output
 
 # Copy local tiles to gc
-gsutil -m cp -r ./data/output gs://nature-watch-bucket/tiles/built/2022
+gsutil -m cp -r /container/data/output gs://nature-watch-bucket/tiles/built/2022
 
 # Delete temporary data
-rm -r ./data/*
+rm -r /container/data/*
