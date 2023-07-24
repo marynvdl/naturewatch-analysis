b19712d533423556f7d7e467070a3c3e385985c0
marynvdl <maryn@laarse.co.za>
2023-07-24 17:45:01 +0200

    Change docker disk mounted and run built

--- tasks/createBareTiles/deploy.sh
+++ tasks/createBareTiles/deploy.sh
@@ -5,4 +5,4 @@ docker rm -f create-bare-tiles || true
 
 # Build docker image and run container
 docker build --no-cache -t create-bare-tiles:1.0.0 .
-docker run -v /etc/keys:/tmp/keys -d --name create-bare-tiles -v /mnt/disks/dataDisk:/container/data create-bare-tiles:1.0.0
+docker run -v /etc/keys:/tmp/keys -d --name create-bare-tiles -v /mnt/disks/dataDisk/task2:/container/data create-bare-tiles:1.0.0
--- tasks/createBuiltTiles/deploy.sh
+++ tasks/createBuiltTiles/deploy.sh
@@ -5,4 +5,4 @@ docker rm -f create-built-tiles || true
 
 # Build docker image and run container
 docker build --no-cache -t create-built-tiles:1.0.9 .
-docker run -v /etc/keys:/tmp/keys -d --name create-built-tiles -v /mnt/disks/dataDisk:/container/data create-built-tiles:1.0.9
+docker run -v /etc/keys:/tmp/keys -d --name create-built-tiles -v /mnt/disks/dataDisk/task1:/container/data create-built-tiles:1.0.9
