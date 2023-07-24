6b53317395141e52a6a614341a19459a9e64229d
marynvdl <maryn@laarse.co.za>
2023-07-24 13:31:52 +0200

    Remove docker container if already exists

--- tasks/createBuiltTiles/deploy.sh
+++ tasks/createBuiltTiles/deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Stop and remove the Docker container if it exists
+docker rm -f create-built-tiles || true
+
 # Build docker image and run container
 docker build --no-cache -t create-built-tiles:1.0.9 .
 docker run -v /etc/keys:/tmp/keys -d --name create-built-tiles -v /mnt/disks/dataDisk:/container/data create-built-tiles:1.0.9
