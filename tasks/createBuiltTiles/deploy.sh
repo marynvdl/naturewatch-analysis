94eec00a77ef810202bca400273451e3ebbc7aa5
marynvdl <maryn@laarse.co.za>
2023-07-24 12:27:15 +0200

    Update path to run workflow

--- tasks/createBuiltTiles/deploy.sh
+++ tasks/createBuiltTiles/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# Build docker image and run container
+# Build docker image & run container
 docker build --no-cache -t create-built-tiles:1.0.9 .
 docker run -v /etc/keys:/tmp/keys -d --name create-built-tiles -v /mnt/disks/dataDisk:/container/data create-built-tiles:1.0.9
