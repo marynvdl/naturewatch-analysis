9c58788852b47f77c4ba8967b9bd8b932d85860c
marynvdl <maryn@laarse.co.za>
2023-07-24 13:39:06 +0200

    Update workflow for bare tiles

--- tasks/createBareTiles/deploy.sh
+++ tasks/createBareTiles/deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# Stop and remove the Docker container if it exists
+docker rm -f create-bare-tiles || true
+
+# Build docker image and run container
 docker build --no-cache -t create-bare-tiles:1.0.0 .
 docker run -v /etc/keys:/tmp/keys -d --name create-bare-tiles -v /mnt/disks/dataDisk:/container/data create-bare-tiles:1.0.0
