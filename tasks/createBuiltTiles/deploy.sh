676d70335b68f6cd5137c07c85fa4217b14a7b40
marynvdl <maryn@laarse.co.za>
2023-08-21 11:37:24 +0200

    Ensure latest image is built

--- tasks/createBuiltTiles/deploy.sh
+++ tasks/createBuiltTiles/deploy.sh
@@ -12,16 +12,12 @@ ls -la ~/
 echo "2. Stopping and removing existing docker container"
 docker rm -f create-built-tiles || true
 
-# Remove the old Docker image if it exists
-echo "2.5 Removing old docker image"
-docker rmi -f create-built-tiles || true
-
 # Build docker image and run container
 echo "3. Create and run docker container"
 echo "Checking if ~/key.json exists..."
 ls -la ~/
 
-docker build --no-cache -t create-built-tiles:1.0.10 .
-docker run -v ~/key.json:/tmp/keys/key.json -v ~/vm_data:/container/data -d --name create-built-tiles create-built-tiles:1.0.9
+docker build --no-cache -t create-built-tiles:1.0.11 .
+docker run -v ~/key.json:/tmp/keys/key.json -v ~/vm_data:/container/data -d --name create-built-tiles create-built-tiles:1.0.11
 
 echo "4. Done with deploy.sh"
