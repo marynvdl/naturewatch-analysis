d47b9df12bee120543b1dcbb741745f00e291b96
marynvdl <maryn@laarse.co.za>
2023-08-21 11:09:20 +0200

    Remove docker image before rebuild

--- tasks/createBuiltTiles/createBuiltTiles.sh
+++ tasks/createBuiltTiles/createBuiltTiles.sh
@@ -10,7 +10,7 @@ gcloud auth activate-service-account --key-file=/tmp/keys/key.json
 
 # Copy all images from gc to local repo
 echo "2. Copying from GC"
-gsutil -m cp gs://nature-watch-bucket/COGS/built/built2022/built2022_15.tif /container/data/input
+gsutil -m cp gs://nature-watch-bucket/COGS/built/built2022/built2022_13.tif /container/data/input
 
 # Build a virtual dataset from all input images
 echo "3. Building virtual dataset"
