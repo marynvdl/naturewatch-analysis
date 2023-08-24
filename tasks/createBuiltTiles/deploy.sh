3289a5cac258ebaf1d5151c093a18fb5cdcda1f1
marynvdl <maryn@laarse.co.za>
2023-08-24 14:16:23 +0200

    Change region of bucket, testing 0-2 zoom levels

--- tasks/createBuiltTiles/deploy.sh
+++ tasks/createBuiltTiles/deploy.sh
@@ -17,7 +17,7 @@ echo "3. Create and run docker container"
 echo "Checking if ~/key.json exists..."
 ls -la ~/
 
-docker build --no-cache -t create-built-tiles:1.0.11 .
-docker run -v ~/key.json:/tmp/keys/key.json -v ~/vm_data:/container/data -d --name create-built-tiles create-built-tiles:1.0.11
+docker build --no-cache -t create-built-tiles:1.0.12 .
+docker run -v ~/key.json:/tmp/keys/key.json -v ~/vm_data:/container/data -d --name create-built-tiles create-built-tiles:1.0.12
 
 echo "4. Done with deploy.sh"
