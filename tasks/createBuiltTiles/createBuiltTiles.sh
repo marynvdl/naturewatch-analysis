04b1c325d45f7ec3ae62b27081fba69de566bd20
marynvdl <maryn@laarse.co.za>
2023-08-25 11:00:51 +0200

    Increase VM disk size to 100GB. Run built tiles for Africa zoom 0-12

--- tasks/createBuiltTiles/createBuiltTiles.sh
+++ tasks/createBuiltTiles/createBuiltTiles.sh
@@ -10,7 +10,7 @@ gcloud auth activate-service-account --key-file=/tmp/keys/key.json
 
 # Copy all images from gc to local repo
 echo "2. Copying from GC"
-gsutil -m cp gs://nature-watch-bucket/COGS/built/built2022/built2022_11.tif  /container/data/input
+gsutil -m cp gs://nature-watch-bucket/COGS/built/built2022/*  /container/data/input
 
 # Build a virtual dataset from all input images
 echo "3. Building virtual dataset"
@@ -26,7 +26,7 @@ gdaldem color-relief /container/data/masked.vrt ./color.txt /container/data/colo
 
 # Make tiles
 echo "6. Make tiles"
-gdal2tiles.py -z 0-2 -s EPSG:4326 -r max -w none -a 0 --xyz /container/data/colored.tif /container/data/output
+gdal2tiles.py -z 0-12 -s EPSG:4326 -r max -w none -a 0 --xyz /container/data/colored.tif /container/data/output
 
 # Copy local tiles to gc
 echo "7. Upload to GC"
