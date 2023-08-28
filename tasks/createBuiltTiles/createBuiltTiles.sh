3a10dd191de3bb34fbbc24db6081735412dfc449
marynvdl <maryn@laarse.co.za>
2023-08-28 17:52:32 +0200

    Testing average resampling for one block, zoom 0-6

--- tasks/createBuiltTiles/createBuiltTiles.sh
+++ tasks/createBuiltTiles/createBuiltTiles.sh
@@ -10,7 +10,7 @@ gcloud auth activate-service-account --key-file=/tmp/keys/key.json
 
 # Copy all images from gc to local repo
 echo "2. Copying from GC"
-gsutil -m cp gs://nature-watch-bucket/COGS/built/built2022/*  /container/data/input
+gsutil -m cp gs://nature-watch-bucket/COGS/built/built2022/built2022_15.tif  /container/data/input
 
 # Build a virtual dataset from all input images
 echo "3. Building virtual dataset"
@@ -26,11 +26,11 @@ gdaldem color-relief /container/data/masked.vrt ./color.txt /container/data/colo
 
 # Make tiles
 echo "6. Make tiles"
-gdal2tiles.py -z 0-6 -s EPSG:4326 -r bilinear -w none -a 0 --xyz /container/data/colored.tif /container/data/output
+gdal2tiles.py -z 0-6 -s EPSG:4326 -r average -w none -a 0 --xyz /container/data/colored.tif /container/data/output
 
 # Copy local tiles to gc
 echo "7. Upload to GC"
-gsutil -m cp -r /container/data/output/* gs://nature-watch-tiles/built/2022_temp
+gsutil -m cp -r /container/data/output/* gs://nature-watch-tiles/built/2022_ave
 
 # Delete temporary data
 rm -r /container/data/*
