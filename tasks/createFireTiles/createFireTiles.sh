270ac5dc21c39f0e1be0042d89d84c96b6f59e33
marynvdl <maryn@laarse.co.za>
2023-09-24 14:12:30 +0200

    Fire tiles 2022 bilinear 0-2 for one: VM size 100GB

---
+++ tasks/createFireTiles/createFireTiles.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Make the temporary data directories
+mkdir -p /container/data/input
+mkdir -p /container/data/output
+
+# Authenticate to Google Cloud Storage
+echo "1. Authenticating GC"
+gcloud auth activate-service-account --key-file=/tmp/keys/key.json
+
+# Copy all images from gc to local repo
+echo "2. Copying from GC"
+gsutil -m cp gs://nature-watch-bucket/COGS/fire/2022/fire2022_13.tif  /container/data/input
+
+# Build a virtual dataset from all input images
+echo "3. Building virtual dataset"
+gdalbuildvrt /container/data/merged.vrt /container/data/input/*.tif
+
+# Mask no data values
+echo "4. Mask no data"
+gdal_translate -a_nodata 0 /container/data/merged.vrt /container/data/masked.vrt -of VRT
+
+# Apply a color-relief to the merged image
+echo "5. Applying color relief"
+gdaldem color-relief /container/data/masked.vrt ./color.txt /container/data/colored.tif -co "COMPRESS=DEFLATE"
+
+# Make tiles
+echo "6. Make tiles"
+gdal2tiles.py -z 0-1 -s EPSG:4326 -r bilinear -w none -a 0 --xyz /container/data/colored.tif /container/data/output
+
+# Copy local tiles to gc
+echo "7. Upload to GC"
+gsutil -m cp -r /container/data/output/* gs://nature-watch-tiles/fire/2022
+
+# Delete temporary data
+rm -r /container/data/*
+
+
+
+
+
---
+++ tasks/createFireTiles/deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "1. Starting deploy.sh"
+echo "Current user: $(whoami)"
+echo "Creating directory..."
+rm -rf ~/vm_data
+mkdir -p ~/vm_data
+echo "Directory created. Checking..."
+ls -la ~/
+
+# Stop and remove the Docker container if it exists
+echo "2. Stopping and removing existing docker container"
+docker rm -f create-fire-tiles || true
+
+# Build docker image and run container
+echo "3. Create and run docker container"
+echo "Checking if ~/key.json exists..."
+ls -la ~/
+
+docker build --no-cache -t create-fire-tiles:1.0.0 .
+docker run -v ~/key.json:/tmp/keys/key.json -v ~/vm_data:/container/data -d --name create-fire-tiles create-fire-tiles:1.0.0
+
+echo "4. Done with deploy.sh"
