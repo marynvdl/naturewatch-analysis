9bb6f14d10765d9778c79ee0a56b1ef0483fe1fe
marynvdl <maryn@laarse.co.za>
2023-10-17 12:37:58 +0200

    Fire tiles 2022 near 0-5 for one binned: VM size 50GB

--- tasks/createFireTiles/deploy.sh
+++ tasks/createFireTiles/deploy.sh
@@ -17,7 +17,7 @@ echo "3. Create and run docker container"
 echo "Checking if ~/key.json exists..."
 ls -la ~/
 
-docker build --no-cache -t create-fire-tiles:1.0.8 .
-docker run -v ~/key.json:/tmp/keys/key.json -v ~/vm_data:/container/data -d --name create-fire-tiles create-fire-tiles:1.0.8
+docker build --no-cache -t create-fire-tiles:1.0.9 .
+docker run -v ~/key.json:/tmp/keys/key.json -v ~/vm_data:/container/data -d --name create-fire-tiles create-fire-tiles:1.0.9
 
 echo "4. Done with deploy.sh"
