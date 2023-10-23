23cdf4c60e2aae5568cd066a60c4b0a8769fd4db
marynvdl <maryn@laarse.co.za>
2023-10-23 19:20:42 +0200

    Built tiles 2023 max 0-5 for one light purple: VM size 10GB: 2023

--- tasks/createBuiltTiles/deploy.sh
+++ tasks/createBuiltTiles/deploy.sh
@@ -17,7 +17,7 @@ echo "3. Create and run docker container"
 echo "Checking if ~/key.json exists..."
 ls -la ~/
 
-docker build --no-cache -t create-built-tiles:1.0.13 .
-docker run -v ~/key.json:/tmp/keys/key.json -v ~/vm_data:/container/data -d --name create-built-tiles create-built-tiles:1.0.13
+docker build --no-cache -t create-built-tiles:1.1.0 .
+docker run -v ~/key.json:/tmp/keys/key.json -v ~/vm_data:/container/data -d --name create-built-tiles create-built-tiles:1.1.0
 
 echo "4. Done with deploy.sh"
